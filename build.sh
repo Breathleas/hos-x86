d08baa98add1f6ccf381c67413f20a7c2bda1571
jaychandra6 <jaypatel6814@gmail.com>
2021-01-26 13:12:08 +0530

    updated build commands

--- build.sh
+++ build.sh
@@ -10,7 +10,8 @@ nasm -f elf ./boot/boot.asm -o ./bin/boot.o
 gcc -m32 -elf_i386 -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./kernel/include -c -o ./bin/main.o ./kernel/main.c
 gcc -m32 -elf_i386 -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./kernel/include -c -o ./bin/screen.o ./kernel/screen.c
 gcc -m32 -elf_i386 -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./kernel/include -c -o ./bin/gdt.o ./kernel/gdt.c
+gcc -m32 -elf_i386 -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./kernel/include -c -o ./bin/idt.o ./kernel/idt.c
 
 
 #linker scripts
-ld -T link.ld -m elf_i386 -o kernel.bin ./bin/boot.o ./bin/main.o ./bin/screen.o ./bin/gdt.o
+ld -T link.ld -m elf_i386 -o kernel.bin ./bin/boot.o ./bin/main.o ./bin/screen.o ./bin/gdt.o ./bin/idt.o
