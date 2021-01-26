91b8e1f7796c5a71a1d0d03168d608dbf584ef9f
jaychandra6 <jaypatel6814@gmail.com>
2021-01-26 13:50:42 +0530

    added isr.c and prototype functions in system.h

--- kernel/include/system.h
+++ kernel/include/system.h
@@ -89,9 +89,24 @@ void scroll_line();
 /* GDT.C */
 void gdt_set_gate(int num, unsigned long base, unsigned long limit, unsigned char access, unsigned char gran);
 void gdt_install();
+
 //-----------------------------------------------------------------------------------------------------------------
 /* IDT.C */
 void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags);
 void idt_install();
+
+//----------------------------------------------------------------------------------------------------------------
+/* ISR.C */
+void isr_install();
+//the following is a data structure that defines how stack looks when an ISR is running
+//as we know, we push the pointer of stack on to the stack when an ISR occurs, to handle those interrutps using our
+//C fault handler we use the following to show how stack looks like.
+struct regs
+{
+    unsigned int gs, fs, es, ds;      /* pushed the segs last */
+    unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;  /* pushed by 'pusha' */
+    unsigned int int_no, err_code;    /* our 'push byte #' and ecodes do this */
+    unsigned int eip, cs, eflags, useresp, ss;   /* pushed by the processor automatically */ 
+};
 
 #endif
